bf931eb489cf28c1e67ca04e36787b73bb8617b8
jadj22 <joelAdjanke@icloud.com>
2024-04-03 11:25:40 +0000

    Ecriture de texte animé,typewrite

---
+++ Day_36/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var app = document.getElementById('typewriter');
+
+    var typewriter = new Typewriter(app, {
+        loop: true
+    });
+
+    typewriter.typeString('Hello,<br> je suis Adjanke Joel')
+        .pauseFor(2500)
+        .deleteAll()
+        .typeString('Découvrez mes projets.')
+        .pauseFor(2500)
+        .deleteAll()
+        .start();
+});
