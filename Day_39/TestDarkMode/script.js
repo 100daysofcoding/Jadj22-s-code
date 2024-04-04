0bc777e01f3b75aa1f661aad01805c8f9987dfa8
jadj22 <joelAdjanke@icloud.com>
2024-04-04 18:46:54 +0000

    Creation de Hamburger-menu et amelio.. apportées

--- Day_39/TestDarkMode/script.js
+++ Day_39/TestDarkMode/script.js
@@ -1,7 +1,7 @@
 let Darkmode = false;
 
 function changeMode(){
-    if(Darkmode){
+    if(Darkmode){//si le dark mode est activé ou appuyé on le met en light mode
         //light mode
         Darkmode = false;
         document.documentElement.style.setProperty("--text-color","black");
@@ -14,5 +14,30 @@ function changeMode(){
         document.documentElement.style.setProperty("--text-color", "white");
         document.documentElement.style.setProperty("--background", "black");
         document.getElementById("dark-light-mode").innerHTML = "dark_mode"
+    }
+}
+
+let FanMode = false;
+
+function changeFan(){
+    if(FanMode) { // si le fan Off est appuyé on le met en met en fan On
+        FanMode = false;
+        document.getElementById("mode-fan").innerHTML = "mode_fan"
+    }else{
+        FanMode = true;
+        document.getElementById("mode-fan").innerHTML = "mode_fan_off"
+    }
+}
+
+let btnMenu = false;
+
+function changeMenu(){
+    if(btnMenu){// si j'appui sur le bouton menu le btn close apparait
+        btnMenu=false;
+        document.getElementById("modeMenu").innerHTML = "close"
+    }
+    else{
+        btnMenu = true;
+        document.getElementById("modeMenu").innerHTML = "menu"
     }
 }
