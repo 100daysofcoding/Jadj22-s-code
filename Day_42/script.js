29cb2798f70611ed25edfd118b715167cb925d2f
jadj22 <joelAdjanke@icloud.com>
2024-04-05 21:25:33 +0000

    modified

--- Day_42/script.js
+++ Day_42/script.js
@@ -16,4 +16,14 @@ function toggleMenu(){
     }
 }
 
+// Ajouter un écouteur d'événements sur le document pour détecter les clics
+document.addEventListener('click', function(event) {
+    // Vérifier si l'élément cliqué n'est pas le menu ni un enfant du menu
+    if (!event.target.closest('#menuList') && !event.target.closest('.menu-icon')) {
+        // Fermer le menu si c'est le cas
+        menuList.style.maxHeight = "0px";
+        modeMenu = true;
+        document.getElementById("modeMenu").innerHTML = "menu"
+    }
+});
 
